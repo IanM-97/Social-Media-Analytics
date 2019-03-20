d2d2974e64e32ac6bdc6244f7d4c980f5493b037
“IanM-97” <Ian.2k11@hotmail.com>
2019-03-20 18:57:54 +0000

    Updated Facebook and Instagram with Value calculations

--- Instagram.py
+++ Instagram.py
@@ -1,6 +1,6 @@
 from InstagramAPI import InstagramAPI
 
-InstagramAPI = InstagramAPI("", "")
+InstagramAPI = InstagramAPI("themanmahon", "Ian_Mahon1")
 InstagramAPI.login()
 
 #InstagramAPI.getProfileData()
@@ -25,9 +25,25 @@ while has_more_posts:
     myposts.extend(InstagramAPI.LastJson['items'])  # merge lists
     time.sleep(2)  # Slows the script down to avoid flooding the servers
 
+totalValue = 0
+totalLikes = 0
+totalComments = 0
+
 for item in myposts:
+    Value = 0
     print("Poster Name: ", item['user']['username'])
     print("Post ID: ",item['id'])
     print("Comment Count: ", item['comment_count'])
     print("Like count: ", item['like_count'])
+    Value += ((item['like_count'] * 0.15) + (item['comment_count'] * 0.10))
+    print("Value: €", round(Value,2))
+    totalValue += Value
+    totalComments += item['comment_count']
+    totalLikes += item['like_count']
+
+print("\n******Totals of all Posts******\n")
+print("Total Likes: ", totalLikes)
+print("Total Comments: ", totalComments)
+print("Total Overall Value: €", round(totalValue,2))
+
 
--- facebookTest.py
+++ facebookTest.py
@@ -1,7 +1,7 @@
 import facebook
 import InstagramAPI
-token = 'EAADm9nbycx4BANuGpBHQKJtNnye1VmgIJiFZCCLYjYIVZCNcZCsTgzJS3RiLEu8ZCFVLh2EK63LDDejTWn0Tm891F0K1MkqSP5vRm13oUawCbzxX0yBIzT8m5AqLaxpTfHJQp7N0BMWIjDzzXyGwnov5zWl4p5OQTovIK9BvN0aAPOgFlbUZAb8B0TuPUmG4ZD'
-
+token = 'EAADm9nbycx4BAIVJYc45g3L1ly0GSXF3vJ6QMh2jlfqjMq3qmGzoKpWsZCDjYT320JKG0G9HZBHNGasFyv33dIVedETZAE4R6eA9riCdRiWRQ52brwUeKNTLtjZBy88HfFuEQ8jInbAxsrmoQEg4WL8gTD95cKyu36J6SRLOlFT2LFo0Gu48dRxZAvqYFIVzS3Sh8kiaHJgZDZD'
+pageToken = "EAADm9nbycx4BANaUIZCK6aaEpA6oom10Kvghu87S86ZBdBDkx4jywGkgz46esJSkcIu8xeBfl0GggRW6OZCMAgGRPp2FqQVUdgQUB8dPjDRSbGROQ03DckiF1jBs7vop8s72yjCbRw6cRHIyHWueMjTBPaj9a57fYizw3M1hHx5TqP2SUAPdyVq8HbX0NatF4FiZCddmZBgZDZD"
 import facebook
 import os
 import jinja2
@@ -33,11 +33,36 @@ import json
 
 mypage = 345279809417210
 
-graph = facebook.GraphAPI(token)
+graph = facebook.GraphAPI(pageToken)
 posts = graph.get_connections(mypage, 'posts')
 
+postNumber = 0
+total_likes = 0
+total_comments = 0
+total_shares = 0
+total_Value = 0
+
 for post in posts['data']:
+    value = 0
+    postNumber += 1
     id = post['id']
-    likes = graph.get_connections(id, connection_name='likes', summary='true')
+    likes = graph.get_connections(id, connection_name='reactions', summary='true')
     comments = graph.get_connections(id, connection_name='comments', summary='true')
-    print(likes['summary'], comments['summary'])
+    print("******************Next Post******************")
+    print("\nPost Number: ", postNumber)
+    print("Time Posted: ", post['created_time'])
+    print("Post: " ,post['message'])
+    print("Reations: ", likes['summary']['total_count'])
+    print("Comments: ", comments['summary']['total_count'])
+    value += (likes['summary']['total_count'] * 0.15) + (comments['summary']['total_count'] * 0.10)
+    print("Value: ", round(value),2)
+
+    total_likes += likes['summary']['total_count']
+    total_comments += comments['summary']['total_count']
+    total_Value += value
+
+print("\n******Total Amounts from all Posts******")
+print("Total amount of Likes: ", total_likes)
+print("Total amount of Comments: ", total_comments)
+print("Total Value: €", round(total_Value),2)
+
